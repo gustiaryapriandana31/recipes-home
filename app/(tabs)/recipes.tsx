fa5070efb66bc5d6a6bea6c99e01c9dac8c041f0
Arya Priandana <gustiaryapriandana@gmail.com>
2026-07-05 09:17:19 +0700

    feat: implement recipes view with tag filtering and list recipes fetch

---
+++ app/(tabs)/recipes.tsx
@@ -0,0 +1,87 @@
+import Header from "@/components/Header";
+import { LinearGradient } from "expo-linear-gradient";
+import { Text, View, Image, StyleSheet, FlatList, Alert } from "react-native";
+import { COLOR } from "../../constants/color";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import RecipeItem from "@/components/RecipeItem";
+import TagItem from "@/components/TagItem";
+
+const RecipesScreen = () => {
+
+    const [recipes, setRecipes] =  useState<any[]>([]);
+    const [tags, setTags] = useState<string[]>([]);
+    const [name, setName] = useState<string>("");
+
+    const getRecipes = async () => {
+        if(!name) {
+            const { data } = await axios.get(`https://dummyjson.com/recipes?limit=50&select=id,name,image,cookTimeMinutes`);
+            setRecipes(data.recipes);
+        } else {
+            const { data } = await axios.get(`https://dummyjson.com/recipes/tag/${name}`);
+            setRecipes(data.recipes);
+        }
+
+    }
+    
+    const getTags = async () => {
+        const { data } = await axios.get(`https://dummyjson.com/recipes/tags`);
+
+        setTags(data);
+    }
+
+    const handleChangeRecipe = async (name:string) => {
+        // Alert.alert(name);
+        setName(name)
+    }
+
+    useEffect(() => {
+        getRecipes();
+        getTags();
+    },[name])
+
+
+    return (
+        <LinearGradient
+            colors={[COLOR.background, COLOR.backgroundLight]}
+            style={{ flex: 1 }}>
+            <Header title="Nusantara Recipes Home" />
+            
+            {/* List Tags */}
+            <View>
+                <FlatList
+                    data={tags}
+                    renderItem={({ item }) => (
+                        <TagItem name={item} handleChangeRecipe={handleChangeRecipe} selectedName={name}/>
+                    )}
+                    keyExtractor={(item) => item}
+                    horizontal
+                    contentContainerStyle={{gap:6}}
+                    style={{marginHorizontal:7,marginBottom:10}}
+                />
+            </View>
+
+            {/* List Recipes */}
+            <View style={{flex:1, margin:5}}>
+                <FlatList
+                    data={recipes}
+                    numColumns={2}
+                    contentContainerStyle={{gap:10}}
+                    columnWrapperStyle={{ justifyContent:'space-between', gap:10 }}
+                    style={{marginTop:10}}
+                    renderItem={({ item }) => (
+                        <RecipeItem
+                            id={item.id}
+                            name={item.name}
+                            cookTimeMinutes={item.cookTimeMinutes}
+                            image={item.image}
+                        />
+                    )}
+                    keyExtractor={(item) => item.id.toString()}
+                />
+            </View>
+        </LinearGradient>
+    );
+};
+
+export default RecipesScreen;
